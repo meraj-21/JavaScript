b2ae1549cc2ad81a92fae72d994d671ee8af151f
Meraj Ahmed <132747992+meraj-21@users.noreply.github.com>
2024-11-19 10:21:30 +0000

    learning Variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144532
+let accountEmail = "meraj@gmail.com" 
+var accountPassword = "12345"
+let accountState;
+
+accountEmail = "ahmed@123mail.com"
+accountPassword = "345677"
+
+/*
+prefare not to use a var
+because of issue a block scope and functional scope
+*/ 
+
+console.table([accountId, accountEmail, accountPassword, accountState])
