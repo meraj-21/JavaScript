4fb6249fd05cc5eb240db74972519e7f0e5a53e9
Meraj Ahmed <132747992+meraj-21@users.noreply.github.com>
2024-11-25 08:39:56 +0000

    Conversion_Operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,76 @@
+let score = "33abc";
+
+// console.log(typeof score); // 33
+// console.log(typeof(score)); // 33
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); // 33
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+
+
+let isLoggedIn = "hitest"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanIsLoggedIn); // boolean
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "hitest" => true
+
+let someNumber = 33
+
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber); // string
+// console.log(stringNumber);
+
+
+
+// ************************ Operators *************************
+
+let value = 3
+let negValue = -value
+//console.log(typeof negValue); // number
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+// console.log(2**2);
+// console.log(2**3);
+
+// console.log(2<<2);
+// console.log(2>>2);
+
+let str1 = "Hello";
+let str2 = " Meraj";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
